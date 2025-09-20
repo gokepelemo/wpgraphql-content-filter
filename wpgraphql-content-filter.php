bf1b9f88bcce5d859026191fa222359cf9593dce
gokepelemo <goke.pelemo@gmail.com>
2025-09-20 16:48:01 -0500

    Bump version to 2.0.3
    - Update plugin version in main file
    - Update readme.txt stable tag
    - Update version constant
    - Prepare for release 2.0.3

--- wpgraphql-content-filter.php
+++ wpgraphql-content-filter.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPGraphQL Content Filter
  * Plugin URI: https://github.com/gokepelemo/wpgraphql-content-filter/
  * Description: Filter and sanitize content in WPGraphQL and REST API responses with configurable HTML stripping, Markdown conversion, and custom tag allowlists. Requires WPGraphQL plugin.
- * Version: 2.0.2
+ * Version: 2.0.3
  * Author: Goke Pelemo
  * Author URI: https://github.com/gokepelemo
  * License: GPL v2 or later
@@ -33,7 +33,7 @@ if (!defined('ABSPATH')) {
 
 // Define plugin constants
 if (!defined('WPGRAPHQL_CONTENT_FILTER_VERSION')) {
-    define('WPGRAPHQL_CONTENT_FILTER_VERSION', '2.0.2');
+    define('WPGRAPHQL_CONTENT_FILTER_VERSION', '2.0.3');
 }
 if (!defined('WPGRAPHQL_CONTENT_FILTER_PLUGIN_FILE')) {
     define('WPGRAPHQL_CONTENT_FILTER_PLUGIN_FILE', __FILE__);
